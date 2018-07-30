a6041c1aa3b1c8280abd51f8fc4892f2f5695702
yemengge <37070681+yemengge@users.noreply.github.com>
2018-07-30 18:02:33 +0800

    仓库初始化

---
+++ springcloud-serviceB/src/main/java/com/springcloud/demo/SpringcloudServicebApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringcloudServicebApplication {
+
+    @GetMapping("serviceB")
+    public String serviceB(){
+        return "serviceB";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudServicebApplication.class, args);
+    }
+}
---
+++ springcloud-serviceB/src/main/java/service/ServiceBController.java
@@ -0,0 +1,21 @@
+package service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceBController {
+    @GetMapping("serviceB")
+    public String serviceB(){
+        return "serviceB";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(ServiceBController.class,args);
+    }
+}
